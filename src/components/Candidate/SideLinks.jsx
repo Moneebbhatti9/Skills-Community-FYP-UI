4c5957508679a1eee543ccaf05ed748c2bb08096
HamzaAliKUndi <hamza.alee83@gmail.com>
2023-05-28 18:58:03 +0500

    Remove Candidate Extra Side Nav Link

--- src/components/Candidate/SideLinks.jsx
+++ src/components/Candidate/SideLinks.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import {
-   AiOutlineHome,
-   AiOutlineFileText,
-   AiOutlineBell,
+  AiOutlineHome,
+  AiOutlineFileText,
+  AiOutlineBell,
 } from "react-icons/ai";
 import { FaUserTie } from "react-icons/fa";
 import { BsBookmark } from "react-icons/bs";
@@ -12,42 +12,40 @@ import { BiMessageDetail } from "react-icons/bi";
 import "./Dashboard/css/Dashboard.css";
 
 const SideLinks = () => {
-   const location = useLocation();
+  const location = useLocation();
 
-   return (
-      <>
-         <div>
-            <Link
-               to="/candidate/dashboard"
-               className={`sideNavLinkDesign ${
-                  location.pathname === "/candidate/dashboard" ? "active" : ""
-               }`}
-            >
-               <span className="d-flex align-items-center px-4">
-                  <AiOutlineHome
-                     style={{ fontSize: "22px", marginRight: "6px" }}
-                  />{" "}
-                  Dashboard
-               </span>
-            </Link>
-         </div>
-         <div className="mt-2">
-            <Link
-               to="/candidate/profile"
-               className={`sideNavLinkDesign ${
-                  location.pathname === "/candidate/profile" ? "active" : ""
-               }`}
-            >
-               <span
-                  className="d-flex align-items-center px-4"
-                  style={{ textAlign: "center" }}
-               >
-                  <FaUserTie style={{ fontSize: "22px", marginRight: "6px" }} />{" "}
-                  <span>My Profile</span>
-               </span>
-            </Link>
-         </div>
-         <div className="mt-2">
+  return (
+    <>
+      <div>
+        <Link
+          to="/candidate/dashboard"
+          className={`sideNavLinkDesign ${
+            location.pathname === "/candidate/dashboard" ? "active" : ""
+          }`}
+        >
+          <span className="d-flex align-items-center px-4">
+            <AiOutlineHome style={{ fontSize: "22px", marginRight: "6px" }} />{" "}
+            Dashboard
+          </span>
+        </Link>
+      </div>
+      <div className="mt-2">
+        <Link
+          to="/candidate/profile"
+          className={`sideNavLinkDesign ${
+            location.pathname === "/candidate/profile" ? "active" : ""
+          }`}
+        >
+          <span
+            className="d-flex align-items-center px-4"
+            style={{ textAlign: "center" }}
+          >
+            <FaUserTie style={{ fontSize: "22px", marginRight: "6px" }} />{" "}
+            <span>My Profile</span>
+          </span>
+        </Link>
+      </div>
+      {/* <div className="mt-2">
             <Link
                to="/candidate/resume"
                className={`sideNavLinkDesign ${
@@ -61,71 +59,61 @@ const SideLinks = () => {
                   My Resume
                </span>
             </Link>
-         </div>
-         <div className="mt-2">
-            <Link
-               to="/candidate/appliedjobs"
-               className={`sideNavLinkDesign ${
-                  location.pathname === "/candidate/appliedjobs" ? "active" : ""
-               }`}
-            >
-               <span className="d-flex align-items-center  px-4">
-                  <CgBriefcase
-                     style={{ fontSize: "22px", marginRight: "6px" }}
-                  />{" "}
-                  Applied Jobs
-               </span>
-            </Link>
-         </div>
-         <div className="mt-2">
-            <Link
-               to="/candidate/jobalerts"
-               className={`sideNavLinkDesign ${
-                  location.pathname === "/candidate/jobalerts" ? "active" : ""
-               }`}
-            >
-               <span className="d-flex align-items-center  px-4">
-                  <AiOutlineBell
-                     style={{ fontSize: "22px", marginRight: "6px" }}
-                  />{" "}
-                  Job Alerts
-               </span>
-            </Link>
-         </div>
-         <div className="mt-2">
-            <Link
-               to="/candidate/jobs/shortlisted"
-               className={`sideNavLinkDesign ${
-                  location.pathname === "/candidate/jobs/shortlisted"
-                     ? "active"
-                     : ""
-               }`}
-            >
-               <span className="d-flex align-items-center  px-4">
-                  <BsBookmark
-                     style={{ fontSize: "22px", marginRight: "6px" }}
-                  />{" "}
-                  Shortlisted Jobs
-               </span>
-            </Link>
-         </div>
-         <div className="mt-2">
-            <Link
-               to="/candidate/messages"
-               className={`sideNavLinkDesign ${
-                  location.pathname === "/candidate/messages" ? "active" : ""
-               }`}
-            >
-               <span className="d-flex align-items-center  px-4">
-                  <BiMessageDetail
-                     style={{ fontSize: "22px", marginRight: "6px" }}
-                  />{" "}
-                  Messages
-               </span>
-            </Link>
-         </div>
-      </>
-   );
+         </div> */}
+      <div className="mt-2">
+        <Link
+          to="/candidate/appliedjobs"
+          className={`sideNavLinkDesign ${
+            location.pathname === "/candidate/appliedjobs" ? "active" : ""
+          }`}
+        >
+          <span className="d-flex align-items-center  px-4">
+            <CgBriefcase style={{ fontSize: "22px", marginRight: "6px" }} />{" "}
+            Applied Jobs
+          </span>
+        </Link>
+      </div>
+      {/* <div className="mt-2">
+        <Link
+          to="/candidate/jobalerts"
+          className={`sideNavLinkDesign ${
+            location.pathname === "/candidate/jobalerts" ? "active" : ""
+          }`}
+        >
+          <span className="d-flex align-items-center  px-4">
+            <AiOutlineBell style={{ fontSize: "22px", marginRight: "6px" }} />{" "}
+            Job Alerts
+          </span>
+        </Link>
+      </div> */}
+      {/* <div className="mt-2">
+        <Link
+          to="/candidate/jobs/shortlisted"
+          className={`sideNavLinkDesign ${
+            location.pathname === "/candidate/jobs/shortlisted" ? "active" : ""
+          }`}
+        >
+          <span className="d-flex align-items-center  px-4">
+            <BsBookmark style={{ fontSize: "22px", marginRight: "6px" }} />{" "}
+            Shortlisted Jobs
+          </span>
+        </Link>
+      </div> */}
+      {/* <div className="mt-2">
+        <Link
+          to="/candidate/messages"
+          className={`sideNavLinkDesign ${
+            location.pathname === "/candidate/messages" ? "active" : ""
+          }`}
+        >
+          <span className="d-flex align-items-center  px-4">
+            <BiMessageDetail style={{ fontSize: "22px", marginRight: "6px" }} />{" "}
+            Messages
+          </span>
+        </Link>
+      </div> */}
+    </>
+  );
 };
 
 export default SideLinks;
